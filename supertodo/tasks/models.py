9372be093df5ade8d8c57e3910b670162242c876
nalleon <nabil14716@gmail.com>
2025-10-08 18:34:25 +0100

    feat(models): Created task model and passed tests

--- supertodo/tasks/models.py
+++ supertodo/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField(max_length=256, blank=True)
+    slug = models.SlugField(unique=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    # def __str__ (self):
+    #     return f'PK:'
+
