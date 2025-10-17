fa9effd19a95ebd34f3fae39eb590056ef828351
nalleon <nabil14716@gmail.com>
2025-10-17 15:24:02 +0100

    style(base): Changed title, pill buttons and subtitle colors

--- supertodo/tasks/models.py
+++ supertodo/tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Task(models.Model):
     name = models.CharField(max_length=70)
     description = models.TextField(max_length=256, blank=True)
@@ -8,6 +9,5 @@ class Task(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
-    # def __str__ (self):
-    #     return f'PK:'
-
+    def __str__(self):
+        return f'PK: {self.pk} | name: {self.name} | description: {self.description} | slug: {self.slug} | completed: {self.completed} | created_at: {self.created_at} | updated_at: {self.updated_at}'
