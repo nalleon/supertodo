d5539397f8862e8add69c6a1458a9db5382c7004
nalleon <nabil14716@gmail.com>
2025-10-17 14:49:56 +0100

    feat(views): Added notfound render in views

--- supertodo/tasks/views.py
+++ supertodo/tasks/views.py
@@ -9,39 +9,41 @@ from django.utils.text import slugify
 import datetime
 
 
-
 def task_list(request):
     tasks = Task.objects.all().order_by('-updated_at')
-    return render(request, 'tasks/tasklist.html', {'tasks': tasks, 'subtitle':'Todas las tareas'})
+    return render(request, 'tasks/tasklist.html', {'tasks': tasks, 'subtitle': 'Todas las tareas'})
+
 
 def task_list_pending(request):
-    pending_tasks = Task.objects.filter(completed = False).order_by('-updated_at')
-    
-    return render(request, 'tasks/tasklist.html', {'tasks': pending_tasks, 'subtitle':'Tareas pendientes'})     
+    pending_tasks = Task.objects.filter(completed=False).order_by('-updated_at')
+
+    return render(
+        request, 'tasks/tasklist.html', {'tasks': pending_tasks, 'subtitle': 'Tareas pendientes'}
+    )
 
 
 def task_list_completed(request):
-    completed_tasks = Task.objects.filter(completed = True).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(completed=True).order_by('-updated_at')
 
-    return render(request, 'tasks/tasklist.html', {'tasks': completed_tasks, 'subtitle':'Tareas completadas'}) 
+    return render(
+        request, 'tasks/tasklist.html', {'tasks': completed_tasks, 'subtitle': 'Tareas completadas'}
+    )
 
 
 def task_detail(request, task_slug: str):
-
     try:
         task = Task.objects.get(slug=task_slug)
     except Task.DoesNotExist:
-            return render(request, 'tasks/task/tasknotexists.html', {'task_slug': task_slug})
+        return render(request, 'tasks/task/tasknotexists.html', {'task_slug': task_slug})
 
-    return render(request, 'tasks/task/taskdetails.html', {'task': task, 'subtitle':'Task Details'})
+    return render(
+        request, 'tasks/task/taskdetails.html', {'task': task, 'subtitle': 'Task Details'}
+    )
 
 
 def add_task(request):
-
     if request.method == 'POST':
-
         if (form := AddTaskForm(request.POST)).is_valid():
-
             task = form.save(commit=False)
             task.slug = slugify(task.name)
             task.completed = False
@@ -49,7 +51,7 @@ def add_task(request):
             task.updated_at = datetime.datetime.now()
             task.save()
 
-            return redirect('tasks:task-list')  
+            return redirect('tasks:task-list')
 
     else:
         form = AddTaskForm()
@@ -58,24 +60,20 @@ def add_task(request):
 
 
 def delete_task(request, task_slug: str):
-
     try:
         task = Task.objects.get(slug=task_slug)
 
     except Task.DoesNotExist:
-        return HttpResponse(f'Task with slug "{task_slug}" does not exist!') # Mejorar esto, quizas un noexiste.html
-    
+        return render(request, 'tasks/task/tasknotexists.html', {'task_slug': task_slug})
     task.delete()
-    return redirect('tasks:task-list')  
+    return redirect('tasks:task-list')
 
 
 def edit_task(request, task_slug: str):
-
     task = Task.objects.get(slug=task_slug)
 
     if request.method == 'POST':
         if (form := EditTaskForm(request.POST, instance=task)).is_valid():
-
             task = form.save(commit=False)
             task.slug = slugify(task.name)
             task.updated_at = datetime.datetime.now()
@@ -85,17 +83,17 @@ def edit_task(request, task_slug: str):
     else:
         form = EditTaskForm(instance=task)
 
-    return render(request, 'tasks/task/taskedit.html', dict(task=task, form=form, subtitle='Editar tarea')) # {task:task, form:form, 'subtitle':'Editar Tarea'} por que no funciona así?
+    return render(
+        request, 'tasks/task/taskedit.html', dict(task=task, form=form, subtitle='Editar tarea')
+    )
 
 
 def toggle_task(request, task_slug: str):
-
     try:
         task = Task.objects.get(slug=task_slug)
 
     except Task.DoesNotExist:
-        return HttpResponse(f'Task with slug "{task_slug}" does not exist!') # Mejorar esto, quizas un noexiste.html
-    
+            return render(request, 'tasks/task/tasknotexists.html', {'task_slug': task_slug})
     task.completed = not task.completed
     task.updated_at = datetime.datetime.now()
     task.save()
